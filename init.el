21da6eb019a364e23f800df3fd53c044a97181f7
Qi Xingli <qixingli@Qis-MacBook-Air.fios-router.home>
2017-03-18 18:41:16 -0400

    add melpa package source, install and configure fill-column-indicator package

--- init.el
+++ init.el
@@ -5,3 +5,16 @@
 (column-number-mode) ; show column number
 
 (xterm-mouse-mode) ; enable mouse
+
+;; add melpa package source
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; install fill-column-indicator if not
+(unless (package-installed-p 'fill-column-indicator)
+  (package-install 'fill-column-indicator))
+(require 'fill-column-indicator)
+(setq-default fci-rule-column 80)
+(add-hook 'prog-mode-hook 'fci-mode) ; enable fci-mode in program mode
