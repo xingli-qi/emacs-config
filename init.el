e0e45869d4224fb1f1d599ffef2f8404172a46ce
Qi Xingli <qixingli@Qis-MacBook-Air.fios-router.home>
2017-03-18 19:40:45 -0400

    set default mode for new buffer, rebind key to list buffers in current window

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
+;; miscellaneous settings
 (setq inhibit-splash-screen t) ; disable splash screen
-
 (global-linum-mode) ; show line number
-
 (column-number-mode) ; show column number
-
 (xterm-mouse-mode) ; enable mouse
+(setq-default major-mode 'text-mode) ; set default mode as text mode
+(global-set-key "\C-x\C-b" 'buffer-menu) ; list buffers in current windows
 
 ;; add melpa package source
 (require 'package)
@@ -12,7 +12,7 @@
 	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; install fill-column-indicator if not
+;; install fill-column-indicator if not, configure and eanble it.
 (unless (package-installed-p 'fill-column-indicator)
   (package-install 'fill-column-indicator))
 (require 'fill-column-indicator)
