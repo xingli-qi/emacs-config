4cdaf7fe5144507ffc4a651139b3861de500bae4
Qi Xingli <qixingli@Qis-MacBook-Air.fios-router.home>
2017-05-02 23:09:48 -0400

    customization

--- init.el
+++ init.el
@@ -41,3 +41,19 @@
 (setq-default sr-speedbar-width 40)
 (setq-default sr-speedbar-max-width 40)
 (setq-default sr-speedbar-right-side nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (misterioso)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#2d3743" :foreground "#e1e1e0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Bitstream Vera Sans Mono")))))
